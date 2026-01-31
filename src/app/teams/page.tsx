bab712b33e7156dd1ef7ac3306fbfa774371687c
PAWAR GUNJAN SINGH <gunjansingh@codnestx.com>
2026-01-31 06:12:20 +0530

    Update page.tsx

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import { Linkedin, Twitter, Mail, Award, Users, Heart, Globe, Code, Brain, Zap, Palette } from 'lucide-react';
 import Image from 'next/image';
 
